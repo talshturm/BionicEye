90f464bfca1421392dbd86af71aff3493906ab61
talshturm <talshturm23@gmail.com>
2024-07-14 17:43:19 +0300

    feat(frame.py): add frame schema

---
+++ Schemas/frame.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class FrameBase(BaseModel):
+    storage_path: str
+    frame_index: int
+
+
+class FrameCreate(FrameBase):
+    video_id: int
+    metadata_id: int
+
+
+class Frame(FrameBase):
+    id: int
+    video_id: int
+    metadata_id: int
+
+    class Config:
+        orm_mode = True
