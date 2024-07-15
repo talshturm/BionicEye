1c59f26606cc0b518e8acd6454e08c6c4f7dd9b3
talshturm <talshturm23@gmail.com>
2024-07-15 17:17:38 +0300

    feat(process_functions): add processing and help functions file

---
+++ utils/process_functions.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+
+def extract_frames(video_path):
+    vidcap = cv2.VideoCapture(video_path)
+    success, image = vidcap.read()
+    frames = []
+    while success:
+        frames.append(image)
+        success, image = vidcap.read()
+    return frames
+
+
+def upload_to_os(frame, video_id, frame_index):
+    s3 = boto3.client('s3')
+    frame_path = f"frames/{video_id}_{frame_index}.jpg"
+    local_path = f"temp/{video_id}_{frame_index}.jpg"
+    cv2.imwrite(local_path, frame)
+    try:
+        s3.upload_file(local_path, "frames", frame_path)
+    except NoCredentialsError:
+        return None
+    os.remove(local_path)
+    return frame_path
