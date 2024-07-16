89576f0b449ec30c011118d4ef907165d9340b29
talshturm <talshturm23@gmail.com>
2024-07-16 18:09:54 +0300

    feat(frame_router): add function to get frame

--- routers/frame_router.py
+++ routers/frame_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from controllers.frame_controller import get_video_frames
+from controllers.frame_controller import get_video_frames, get_frame
 
 router = APIRouter()
 
@@ -11,5 +11,14 @@ def get_frames_paths(video_id: int, db: Session = Depends(get_db)) -> list[str]:
     try:
         frames_paths = get_video_frames(video_id, db)
         return frames_paths
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/videos/{video_id}/frames/{frame_index}")
+def get_frames_paths(video_id: int, frame_index: int, db: Session = Depends(get_db)) -> list[str]:
+    try:
+        frame_paths = get_frame(video_id, frame_index, db)
+        return frame_paths
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
