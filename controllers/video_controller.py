882fdc3093f128481f56b96e0fbaf18e20c12d2f
talshturm <talshturm23@gmail.com>
2024-07-16 12:24:14 +0300

    feat(video_controller): add get paths function

--- controllers/video_controller.py
+++ controllers/video_controller.py
@@ -1,6 +1,11 @@
+from typing import Dict
 from sqlalchemy.orm import Session
-from services.video_service import upload_video_service
+from services.video_service import upload_video_service, get_paths_service
 
 
-async def upload_video(local_path: str, db: Session) -> None:
+async def upload_video(local_path: str, db: Session) -> dict[str, str]:
     return await upload_video_service(local_path, db)
+
+
+async def get_paths(dc: Session) -> None:
+    return await get_paths_service(db)
