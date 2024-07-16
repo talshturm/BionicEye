e2502917e6d7b7be24b2000035f8e3e4a667d839
talshturm <talshturm23@gmail.com>
2024-07-16 18:13:02 +0300

    feat(frame_service): add function to get frame

--- services/frame_service.py
+++ services/frame_service.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from repositories.metadata_repository import create_metadata_repo
 from utils import generate_metadata, is_frame_tagged
 from utils.process_functions import upload_frame_to_os
-from repositories.frame_repository import create_frame_repo, get_frames_repo
+from repositories.frame_repository import create_frame_repo, get_frames_repo, get_frame_repo
 
 
 async def create_frame_service(index: int, frame, video_id: int, db: Session) -> None:
@@ -24,3 +24,7 @@ async def create_frame_service(index: int, frame, video_id: int, db: Session) ->
 
 def get_frames_service(video_id: int, db: Session) -> list[str]:
     return get_frames_repo(video_id, db)
+
+
+def get_frame_service(video_id: int, frame_index: int, db: Session) -> str:
+    return get_frame_repo(video_id, frame_index, db)
