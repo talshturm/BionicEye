e1d3a15e7ae8456541d75dc24b133d823934e165
talshturm <talshturm23@gmail.com>
2024-07-16 12:22:33 +0300

    feat(video_router): add get paths function

--- routers/video_router.py
+++ routers/video_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from controllers.video_controller import upload_video
+from controllers.video_controller import upload_video, get_paths
 
 router = APIRouter()
 
@@ -11,5 +11,14 @@ async def upload(local_path: str, db: Session = Depends(get_db)):
     try:
         await upload_video(local_path, db)
         return {"message": "Video processed successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/videos")
+async def get_all_paths(db: Session = Depends(get_db)):
+    try:
+        await get_paths(db)
+        return {"message": "paths returned successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
