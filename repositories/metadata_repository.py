b84ef0e7340ed3ba53ebbc9605928c34fa05bd0e
talshturm <talshturm23@gmail.com>
2024-07-17 17:07:42 +0300

    feat(metadata_repository): add logging

--- repositories/metadata_repository.py
+++ repositories/metadata_repository.py
@@ -1,15 +1,21 @@
 from typing import Type
-
 from sqlalchemy.orm import Session
-
-from models import Metadata
 from models.metadata import Metadata
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S',
+                    filename='log_file.log',
+                    level=logging.INFO)
 
 
-def create_metadata_repo(tag: bool, fov: float, azimuth: float, elevation: float, db: Session) -> Type[
-                                                                                                      Metadata] | Metadata:
+def create_metadata_repo(tag: bool, fov: float, azimuth: float, elevation: float, db: Session) -> (
+        Type[Metadata] | Metadata):
+    logger.info("trying to add metadata to db")
     existing_metadata = get_existing_metadata(tag, fov, azimuth, elevation, db)
     if existing_metadata:
+        logger.info("metadata already exists")
         return existing_metadata
 
     metadata = Metadata(frame_tag=tag, fov=fov, azimuth=azimuth, elevation=elevation)
@@ -19,8 +25,8 @@ def create_metadata_repo(tag: bool, fov: float, azimuth: float, elevation: float
     return metadata
 
 
-def get_existing_metadata(tag: bool, fov: float, azimuth: float, elevation: float, db: Session) -> Type[
-                                                                                                       Metadata] | None:
+def get_existing_metadata(tag: bool, fov: float, azimuth: float, elevation: float, db: Session) -> (
+        Type[Metadata] | None):
     return db.query(Metadata).filter_by(frame_tag=tag,
                                         fov=fov,
                                         azimuth=azimuth,
