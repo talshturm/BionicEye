9379480b229624f698734cdce61e0b661dec9cc7
talshturm <talshturm23@gmail.com>
2024-07-17 10:31:30 +0300

    feat(frames_router): add prefix to router

--- routers/frame_router.py
+++ routers/frame_router.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 from database import get_db
 from controllers.frame_controller import get_video_frames, get_frame
 
-router = APIRouter()
+router = APIRouter(prefix="/videos/{video_id}/frames")
 
 
-@router.get("/videos/{video_id}/frames")
+@router.get("/")
 def get_frames_paths(video_id: int, db: Session = Depends(get_db)) -> list[str]:
     try:
         frames_paths = get_video_frames(video_id, db)
@@ -15,7 +15,7 @@ def get_frames_paths(video_id: int, db: Session = Depends(get_db)) -> list[str]:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/videos/{video_id}/frames/{frame_index}")
+@router.get("/{frame_index}")
 def get_frames_paths(video_id: int, frame_index: int, db: Session = Depends(get_db)) -> str:
     try:
         frame_paths = get_frame(video_id, frame_index, db)
