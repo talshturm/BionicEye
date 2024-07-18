d1ad08920206edab76575c0c004293d4879c5abf
talshturm <talshturm23@gmail.com>
2024-07-18 15:16:26 +0300

    feat(video_repository_test): add test functions to repository

--- tests/video_repository_test.py
+++ tests/video_repository_test.py
@@ -1,7 +1,9 @@
 import pytest
+from sqlalchemy.exc import IntegrityError
+
 from models import Video
 from test_database import db_session, db_engine
-from repositories.video_repository import create_video_repo
+from repositories.video_repository import create_video_repo, get_video_path_repo
 
 
 def test_create_video_valid(db_session):
@@ -29,7 +31,7 @@ def test_create_video_null_value(db_session):
 
     try:
         create_video_repo(video, db_session)
-    except Exception:
+    except IntegrityError:
         db_session.rollback()
 
 
@@ -42,5 +44,34 @@ def test_create_video_missing_field(db_session):
 
     try:
         create_video_repo(video, db_session)
-    except Exception:
+    except KeyError:
+        db_session.rollback()
+
+
+def test_get_video_path_valid_input(db_session):
+    video_id = 1
+    expected = "videos/TelAviv_15_06_34_12_06_00.mp4"
+
+    actual = get_video_path_repo(video_id, db_session)
+
+    assert expected == actual
+
+
+@pytest.mark.xfail
+def test_get_video_path_null_value(db_session):
+    video_id = None
+
+    try:
+        get_video_path_repo(video_id, db_session)
+    except TypeError:
+        db_session.rollback()
+
+
+@pytest.mark.xfail
+def test_get_video_path_non_existing_video(db_session):
+    video_id = 7
+
+    try:
+        get_video_path_repo(video_id, db_session)
+    except TypeError:
         db_session.rollback()
